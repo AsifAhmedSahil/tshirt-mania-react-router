21db49bbba63f006e8200b00bba5f783e3d96e00
EngineerSahil <77582744+EngineerSahil@users.noreply.github.com>
2022-10-08 12:29:15 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { createBrowserRouter, RouterProvider} from 'react-router-dom'
+import Main from './layout/Main';
+// import Orders from './components/Orders/Orders';
+import About from './components/About/About';
+import Orders from './components/Orders/Orders';
+import Home from './components/Home/Home';
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path:'/',
+      element:<Main/>,
+      children:[
+        {
+          path:'/',
+          loader: ()=> fetch('tshirts.json'),
+          element:<Home/>
+        },
+        {
+          path:'/about',
+          element:<About/>
+        },
+        {
+          path:'/orders',
+          element:<Orders/>
+        },
+      ]
+    }
+  ])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <RouterProvider router={router}>
+
+      </RouterProvider>
+      
     </div>
   );
 }
---
+++ src/components/Home/Home.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { useLoaderData } from "react-router-dom";
+import Cart from "../Cart/Cart";
+import Tshirt from "../Tshirt/Tshirt";
+import './Home.css'
+
+const Home = () => {
+  const tshirts = useLoaderData();
+  return (
+    <div className="home-container">
+      <div className="tshirt-container">
+      {tshirts.map((tshirt) => (
+        <Tshirt key={tshirt.id} tshirt={tshirt}></Tshirt>
+      ))}
+      </div>
+      <div className="cart container">
+        <Cart></Cart>
+
+      </div>
+    </div>
+  );
+};
+
+export default Home;
