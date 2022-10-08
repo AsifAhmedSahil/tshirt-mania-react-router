3b5f4c8ef675e8e8969698b5c062e8c15ce5502a
EngineerSahil <77582744+EngineerSahil@users.noreply.github.com>
2022-10-08 15:53:53 +0600

    Product and cart funtionality done

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,9 +1,22 @@
 import React from 'react'
 
-const Cart = () => {
+const Cart = ({cart,handleRemoveCart}) => {
   return (
     <div>
-        Order Summary
+        <h1>Order Summary </h1>
+        <h2>Cart Item: : {cart.length}</h2>
+
+        {
+          cart.map((tshirt)=>(
+            <div>
+              <p key={tshirt._id}>
+                {tshirt.name}
+                <button onClick={()=>handleRemoveCart(tshirt)}>X</button>
+                </p>
+              
+            </div>
+          ))
+        }
     </div>
   )
 }
--- src/components/Tshirt/Tshirt.js
+++ src/components/Tshirt/Tshirt.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import "./Tshirt.css"
 
-const Tshirt = ({tshirt}) => {
+const Tshirt = ({tshirt,handleTshirtCart}) => {
     const {picture,name,price} = tshirt;
   return (
     <div className='tshirt-details'>
         <img src={picture} alt="" />
         <h2>{name}</h2>
         <h4>{price}</h4>
-        <button>Buy This</button>
+        <button onClick={()=>handleTshirtCart(tshirt)}>Buy This</button>
     </div>
   )
 }
